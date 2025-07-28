49fc2e065b534c5db03f071a2a2e7e452c78e27c
n00tsec <muhammadal241228@gmail.com>
2025-07-28 22:16:16 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
-# setup.py 
+# setup.py
 import os
 import sys
 from setuptools import setup, Extension
 
-import pybind11
+try:
+    import pybind11
+    PYBIND11_INCLUDES = [pybind11.get_include()]
+except ImportError:
+    PYBIND11_INCLUDES = []
 
 if sys.platform == 'win32':
     VCPKG_ROOT = os.environ.get("VCPKG_ROOT", ".")
@@ -15,17 +19,13 @@ else:
     library_dirs = []
     libraries = ["jpeg", "ssl", "crypto"]
 
-
 f5_stego_module = Extension(
     name="Arcova.f5_stego",
     sources=[
         "Arcova/f5_steg/F5_stego_binding.cpp",
         "Arcova/f5_steg/F5steg.cpp"
     ],
-    include_dirs=[
-        pybind11.get_include(),
-        *include_dirs
-    ],
+    include_dirs=[*PYBIND11_INCLUDES, *include_dirs],
     library_dirs=library_dirs,
     libraries=libraries,
     language="c++"
@@ -38,5 +38,5 @@ setup(
     packages=["Arcova", "Arcova.f5_steg"],
     ext_modules=[f5_stego_module],
     zip_safe=False,
-    setup_requires=['pybind11>=2.6', 'setuptools>=42', 'wheel'],
+    setup_requires=['pybind11>=2.6'],
 )
